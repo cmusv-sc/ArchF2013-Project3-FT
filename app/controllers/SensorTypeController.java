c19a83a5cebb37b7b2c7e75fb8c12d6d8644a945
Lyman Cao <caoyifanlyman@gmail.com>
2013-11-19 12:14:43 -0800

    adding new files

---
+++ app/controllers/SensorTypeController.java
@@ -0,0 +1,23 @@
+
+package controllers;
+
+
+import models.metadata.SensorType;
+import play.data.Form;
+import play.mvc.*;
+import views.html.*;
+
+public class SensorTypeController extends Controller {
+	final static Form<SensorType> sensorTypeForm = Form.form(SensorType.class);
+	
+    public static Result sensorTypes() {
+    	return ok(sensorTypes.render(SensorType.all(),sensorTypeForm));
+    }
+    public static Result newSensorType() {
+    	return TODO;
+    }
+    public static Result deleteSensorType(Long id) {
+        return TODO;
+    }
+}
+    
