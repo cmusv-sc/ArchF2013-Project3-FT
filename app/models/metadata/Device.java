5377815ee48f27e3925cd04da43fba86801fc1b8
Yazhisai Gowthaman <yazhisai.gowthaman@sv.cmu.edu>
2013-12-05 17:19:24 -0800

    Device

--- app/models/metadata/Device.java
+++ app/models/metadata/Device.java
@@ -5,6 +5,7 @@ import java.util.*;
 import util.APICall;
 
 
+
 //import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.*;
 
@@ -34,33 +35,31 @@ public class Device {
 	 * @return List<Device> List of all devices
 	 */
 	public static List<Device> all() {
-		
+		System.out.println ("Started here!");
 		List<Device> allDevices = new ArrayList<Device>();
 
 		// API Call: http://einstein.sv.cmu.edu/get_devices/json
 		final JsonNode devicesNode = APICall.callAPI(API_CALL);		
-		
+				
 		// If no value is returned
 		if (devicesNode == null) {
-			return allDevices;
+			return null;
 		}		
 		
+		for (int i=0;i<devicesNode.size();i++) {
+			 JsonNode json = devicesNode.path(i);
+			 Device newDevice = new Device();
+			 
+			 DeviceType deviceType = new DeviceType();
+			 deviceType.setDeviceTypeName(json.findPath("device_type").asText());
 
-		// Not Reached code below!!
-//		String[] devices = devicesNode.path("device_type").toString().replaceAll("\"","").split(",");		
-//		
-//		for (int i=0; i<devices.length; i++) {
-//			
-//			Device device = new Device();
-//			
-//			device.setId(new Long(i)); // temporary id generation
-//						
-//			allDevices.add(device);
-//			//System.out.println(devices[i]);
-//		}
-					
+			 newDevice.setDeviceType(deviceType);
+			 newDevice.setUri(json.findPath("uri").asText());
+			 
+			 System.out.println ("Device is" + newDevice);
+			 allDevices.add(newDevice);
+		 }					
 		return allDevices;
-
 	}
 	
 	// Getters
