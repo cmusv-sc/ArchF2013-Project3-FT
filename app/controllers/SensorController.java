b9bf42399ded77381d1916856c997405e25b9cf2
Isil Demir <isil.demir@sv.cmu.edu>
2013-12-05 20:10:58 -0800

    Sensors create form

--- app/controllers/SensorController.java
+++ app/controllers/SensorController.java
@@ -1,18 +1,20 @@
 
 package controllers;
 
+import models.BugReport;
 import models.metadata.Sensor;
 import play.data.Form;
 import play.mvc.*;
 import views.html.*;
 
 public class SensorController extends Controller {
-	final static Form<Sensor> sensorForm = Form.form(Sensor.class);
+	final static Form<Sensor> sensorForm = Form.form(Sensor.class);	
 	
     public static Result sensors() {
     	return ok(sensor.render(Sensor.all(),sensorForm));
     }
     public static Result newSensor() {
+    	Form<Sensor> filledForm = sensorForm.bindFromRequest();
     	return TODO;
     }
     public static Result deleteSensor(String id) {
