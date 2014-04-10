07b905f99331c4add720dff4c6ee276220de9400
Lyman Cao <caoyifanlyman@gmail.com>
2014-04-09 17:51:01 -0700

    fix vfile place

--- app/controllers/EstimateController.java
+++ app/controllers/EstimateController.java
@@ -6,8 +6,8 @@ import views.html.estimator.*;
 
 public class EstimateController extends Controller {
 
-	public static Result estimate(String email, String vfile, String scriptFile) {
-		return ok(estimate.render(email, vfile, scriptFile));
+	public static Result estimate(String email, String vfile, String dataset) {
+		return ok(estimate.render(email, vfile, dataset));
 	}
 	
 	public static Result estimate1() {
